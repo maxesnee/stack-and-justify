17ad955db8800dd3fc8a40f0abccdef01d2cafbc
Max Esnée <contact@max-esnee.com>
2024-01-03 16:33:56 +0100

    Add tooltip on copy button

--- js/components/CopyButton.js
+++ js/components/CopyButton.js
@@ -1,7 +1,23 @@
 export function CopyButton(initialVnode) {
+	const copyStr = "Copy to clipboard";
+	const copiedStr = "Copied";
+
+	function onclick(e, vnode) {
+		vnode.dom.querySelector('.copy-tooltip').textContent = copiedStr;
+
+		vnode.attrs.onclick(e);
+	}
+
+	function onmouseleave(e, vnode) {
+		vnode.dom.querySelector('.copy-tooltip').textContent = copyStr;
+	}
+
 	return {
 		view: function(vnode) {
-			return m('button.copy-button', {onclick: vnode.attrs.onclick}, '📋')
+			return m('div.copy-button',
+				m('button', {onclick: (e) => onclick(e, vnode), onmouseleave: (e) => onmouseleave(e, vnode)}, '📋'),
+				m('div.copy-tooltip', copyStr)
+			)
 		}
 	}
 }
