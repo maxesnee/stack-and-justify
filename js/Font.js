b2f6b6a615c94a02e7378d9fe404d96c6cb52e5d
Max Esnée <contact@max-esnee.com>
2024-03-14 14:32:25 +0100

    Add Typr to parse font files and get metadatas from the fonts

--- js/Font.js
+++ js/Font.js
@@ -8,6 +8,7 @@ export const Font = function(name, data) {
 	const wordGenerator = WordGenerator(fontFaceName, data);
 	let isLoading = true;
 	const id = generateUID();
+	let info;
 
 	async function init() {
 		const fontFace = new FontFace(fontFaceName, data);
@@ -26,6 +27,16 @@ export const Font = function(name, data) {
 		// Dispatch event
 		const event = new CustomEvent("font-loaded", {detail: {fontId: id}});
 		window.dispatchEvent(event);
+
+		// Parse font file to get useful font infos
+		const font = Typr.parse(data)[0];
+		const fsSelection = font["OS/2"].fsSelection.toString(2).padStart(16, "0").split('').reverse();
+		info = {
+			name: font.name.fullName,
+			isItalic: fsSelection[0] === "0" ? false : true,
+			weightClass: font["OS/2"].usWeightClass,
+			widthClass: font["OS/2"].usWidthClass
+		}
 	}
 
 	async function update() {
@@ -47,6 +58,7 @@ export const Font = function(name, data) {
 		},
 		fontFaceName,
 		data,
+		info,
 		id,
 		update,
 		get isLoading() {
